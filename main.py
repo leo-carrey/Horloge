4cec2a79a85c38b095e860195a6f475aed3c9ec2
leo-carrey <leo.carrey@laplateforme.io>
2023-03-17 17:42:46 +0100

    [ADD] clock

--- main.py
+++ main.py
@@ -1,58 +1,68 @@
 import time
 
-temps = True
 
-def clock(hour, minute, seconde):
+def set_time():
+    h = int(input("set hour : "))
+    m = int(input("set minute : "))
+    s = int(input("set second : "))
+    return h, m, s
 
-    temps = True
-    hour = int(input("Enter hour :")) # valeur entrée par Utilisateur
-    minute = int(input("Enter minute :"))
-    seconde = int(input("Enter seconde :"))
-    alarm_seconde = ""
-    alarm_hour = ""
-    alarm_minute = ""
-    h_empty=""
-    m_empty=""
-    s_empty=""
 
-    print("Voulez-vous programmez un reveil ? (y/n)" )
-    alarme = input()
+def set_alarm():
+    h = int(input("set alarm hour : "))
+    m = int(input("set alarm minute : "))
+    s = int(input("set alarm second : "))
+    return h, m, s
 
-    if alarme == "y":
-        print("Enter hour :")
-        alarm_hour = int(input())
-        print("Enter minute :")
-        alarm_minute = int(input())
-        print("Enter seconde :")
-        alarm_seconde = int(input())
 
-    while temps:
+def alarm():
+    userinput = input("you want set an alarm (y/n) : ")
+    if userinput == "y":
+        return set_alarm()
+    else:
+        return None, None, None
+
+
+def run_time(hour, minute, second):
+    second += 1
+    if second >= 60:
+        minute += 1
+        second = 0
+    if minute > 59:
+        hour += 1
+        minute = 0
+    if hour > 23:
+        hour = 0
+    return hour, minute, second
+
+
+def print_time(hour, minute, second):
+    print('Time:{:02d}:{:02d}:{:02d}              '.format(
+        hour, minute, second), end='\r')
+
+
+def main():
+    format = input("choose your time format (24/12) :")
+    alarm_hour, alarm_minute, alarm_second = alarm()
+    hour, minute, second = set_time()
+    while True:
         time.sleep(1)
-        seconde += 1
-        if seconde > 59:
-            minute += 1
-            seconde = 0
-        if minute > 59:
-            hour += 1 # valeur entrée par utilisateur + 1
-            minute = 0
-        if hour > 23:
-            hour = 0
-        if hour <= 9:
-            h_empty = '0'
-        else:
-            h_empty = ''
-        if minute <= 9:
-            m_empty = '0'
-        else:
-            m_empty = ''
-        if seconde <= 9:
-            s_empty = '0'
-        else:
-            s_empty = ''
+        hour, minute, second = run_time(hour, minute, second)
+        if format == "24":
+            print_time(hour, minute, second)
+        elif f'{alarm_hour}:{alarm_minute}:{alarm_second}' == f'{hour}:{minute}:{second}':
+            print_time(hour, minute, second)
+            print('Its Time !!!! :  ')
+        if format == "12":
+            if hour > 12:
+                mini_hour = 0
+                print_time(mini_hour, minute, second)
+                if alarm_hour == mini_hour and alarm_minute == minute and alarm_second == second:
+                    print_time(mini_hour, minute, second)
+            else:
+                print_time(hour, minute, second)
+                if alarm_hour == hour and alarm_minute == minute and alarm_second == second:
+                    print_time(hour, minute, second)
 
-        if alarm_hour == hour and alarm_minute == minute and alarm_seconde == seconde:
-            print("time: " + h_empty + str(hour) + ":" + m_empty + str(minute) + ":" + s_empty + str(seconde), "it's time !!!", end='\r')    
-        else:
-            print("time: " + h_empty + str(hour) + ":" + m_empty + str(minute) + ":" + s_empty + str(seconde), "              ",end="\r")
 
-clock(00,00,00)
+main()
